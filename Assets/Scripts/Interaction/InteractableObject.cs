cc0f5c7479de747e8fa66ccb4b27e3f8112c3b13
javiarias <fezkip@gmail.com>
2023-09-19 22:13:10 +0200

    arreglos urp 2.0

--- Assets/Scripts/Interaction/InteractableObject.cs
+++ Assets/Scripts/Interaction/InteractableObject.cs
@@ -1,15 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class InteractableObject : MonoBehaviour
 {
     public bool canInteract;
 
+    public Outline outline;
+
     // Start is called before the first frame update
     void Start()
     {
-        //glowControl = gameObject.GetComponent<GlowObjectCmd>();
+        outline = gameObject.GetComponent<Outline>();
     }
 
     // Update is called once per frame
