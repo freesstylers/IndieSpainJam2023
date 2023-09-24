702fe337a3cf7b6110a10ad4f475ae7cff89b0e9
Posna <jomart17@ucm.es>
2023-09-24 18:28:03 +0200

    Fusion de items desde inventario

--- Assets/Scripts/InventoryMenu/AutoBuildItemMenu.cs
+++ Assets/Scripts/InventoryMenu/AutoBuildItemMenu.cs
@@ -3,16 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class AutoBuildItemMenu : MonoBehaviour, IPointerDownHandler, IDragHandler
+public class AutoBuildItemMenu : MonoBehaviour, IPointerDownHandler, IDragHandler, IEndDragHandler, IDropHandler, IBeginDragHandler
 {
-
-    //Scriptable con el objeto con la info
-    public delegate void FillItem(string desc, string name, Sprite sprite);
-    public FillItem fi;
-
     private Item item;
 
+    private Vector2 initRect;
     private RectTransform rectTransform;
+    private Canvas canvas;
+    private CanvasGroup canvasGroup;
+    private ItemMenuController itemMenu;
 
     private void Awake()
     {
@@ -22,7 +21,8 @@ public class AutoBuildItemMenu : MonoBehaviour, IPointerDownHandler, IDragHandle
     // Start is called before the first frame update
     void Start()
     {
-        
+        canvas = GetComponentInParent<Canvas>(true);
+        canvasGroup = GetComponent<CanvasGroup>();
     }
 
     // Update is called once per frame
@@ -31,27 +31,48 @@ public class AutoBuildItemMenu : MonoBehaviour, IPointerDownHandler, IDragHandle
         
     }
 
-    public void Fill(Item item_)
+    public void Fill(Item item_, ItemMenuController itemMenu_)
     {
         item = item_;
+        itemMenu = itemMenu_;
         GetComponent<UnityEngine.UI.Button>().image.sprite = item_.sprite;
     }
 
     public void SendInfo()
     {
-        if (fi == null)
+        if (itemMenu == null)
             return;
-
-        fi.Invoke(item.description, item.name, item.sprite);
+        itemMenu.FillFullItem(item.description, item.name, item.sprite);
     }
 
-    void IPointerDownHandler.OnPointerDown(PointerEventData eventData)
+    public void OnPointerDown(PointerEventData eventData)
     {
-        
+        initRect = rectTransform.anchoredPosition;
     }
 
     public void OnDrag(PointerEventData eventData)
     {
-        rectTransform.anchoredPosition += eventData.delta;
+        rectTransform.anchoredPosition += eventData.delta / canvas.scaleFactor;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        canvasGroup.blocksRaycasts = true;
+        rectTransform.anchoredPosition = initRect;
+
+    }
+
+    public void OnDrop(PointerEventData eventData)
+    {
+        if (eventData.pointerDrag != null && eventData.pointerDrag != gameObject)
+        {
+            string id = eventData.pointerDrag.GetComponent<AutoBuildItemMenu>().item.id;
+            itemMenu.CombineItems(id, item.id);
+        }
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        canvasGroup.blocksRaycasts = false;
     }
 }
